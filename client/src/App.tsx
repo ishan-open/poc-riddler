1589c55ac5daa19e36bc3f22a67de5d76c2d06e2
Amir.H Ebrahimi <amirhosseinebrahimi77@gmail.com>
2022-05-05 05:58:44 +0430

    feat: Add basic score-board tracker

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,42 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
 const socket = io(import.meta.env.VITE_WEB_SOCKET);
-
+interface User {
+  userId: string;
+  score: number;
+}
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [user, setUser] = useState<User>({
+    userId: new Date().getTime().toString(),
+    score: 0,
+  });
+  useEffect(() => void socket.on('users', setUsers), []);
   useEffect(() => {
-    socket.on('hello', console.log.bind(console, 'socket.on[hello]'));
-  }, []);
+    const currentUser = users.find(u => u.userId === user.userId);
+    if (currentUser) setUser(currentUser);
+  }, [users]);
 
+  const score = Math.ceil(Math.random() * 30 - 15);
   return (
     <div>
-      <button onClick={() => socket.emit('good', 'boy')}>send</button>
+      <button onClick={() => socket.emit('update-user', user)}>register</button>
+      <button
+        onClick={() =>
+          socket.emit('update-user', { ...user, score: user.score + score })
+        }
+      >
+        score += {score}
+      </button>
+      <hr />
+      <ul>
+        {users.map(user => (
+          <li key={user.userId}>
+            <strong>{user.userId}</strong>: <span>{user.score}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- server/socket.ts
+++ server/socket.ts
@@ -1,4 +1,5 @@
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { DefaultEventsMap } from 'socket.io/dist/typed-events';
 
 const socketPort = parseInt(process.env.SOCKET_PORT!);
 const io = new Server(socketPort, {
@@ -6,10 +7,26 @@ const io = new Server(socketPort, {
     origin: [`${process.env.FRONT_END_HOST}:${process.env.FRONT_END_PORT}`],
   },
 });
+const users: Record<string, User> = {};
+interface User {
+  userId: string;
+  score: number;
+}
 
-io.on('connect', socket => {
-  console.log('connected');
+io.on('connection', (socket: any) => {
+  console.log('Made socket connection');
 
-  socket.emit('hello', 'world');
-  socket.on('good', console.log.bind(console, '[good]::'));
+  socket.on('update-user', function (data: User) {
+    console.log('on new-user');
+
+    socket.userId = data.userId;
+    users[data.userId] = data;
+
+    io.emit('users', Object.values(users));
+  });
+
+  socket.on('disconnect', () => {
+    delete users[socket.userId];
+    io.emit('user-disconnected', socket.userId);
+  });
 });
